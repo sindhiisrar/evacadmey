7b9ed23e0552297985525d6751a3f012bc979ca6
sindhiisrar <israrsindhi01@gmail.com>
2025-06-23 15:34:32 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// firebase.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOkwlDLTtnsNBmPtTDScUllqL1b6hug9c",
+  authDomain: "eduverse-academy-4b96b.firebaseapp.com",
+  projectId: "eduverse-academy-4b96b",
+  storageBucket: "eduverse-academy-4b96b.firebasestorage.app",
+  messagingSenderId: "19810288427",
+  appId: "1:19810288427:web:b837708c33dcc3e8dbbb6e",
+  measurementId: "G-HY59ENKB7P"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
